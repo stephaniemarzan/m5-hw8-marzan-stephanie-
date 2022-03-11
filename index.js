cbb727b2f72045c1bbc7e44b0968ce0d174ec7d9
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2022-03-11 15:36:45 -0600

    added logic to for loop

--- index.js
+++ index.js
@@ -22,13 +22,37 @@ var message = document.getElementById('message');
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
+// create a random number between 1-3 and store it to a variable
+// This number will represent the winning box
+// determine if the box clicked is equal to the random number
+// if the numbers match, display a winning message by changing the text content of the div#message element
+// if the numbers match, increment wins and display the win count in div#wins
+// if the numbers don't match, change the div#message element's text to a random losing message from the array above
+// if the numbers don't match, increment losses and display the loss count in div#losses
+
 
 var boxes = document.querySelectorAll('.box');
+
+randomNumber = Math.floor(Math.random()*4);
 
 for (var i = 0; i < boxes.length; i ++ ){
     boxes[i].addEventListener('click', function(e){
         var boxClicked = e.target.textContent;
         parseInt(boxClicked);
+
+        if(boxClicked = randomNumber){
+            message.textContent = "Congratulations!";
+            wins.textContent = 'Total Wins:'+ totalWins + 1;
+            losses.textContent = 'Total Losses:' +totalLoss;
+        }
+
+        else {
+            for(var i = 0; i < losingMessages.length; i++){
+             message.textContent = losingMessages[i];
+            }
+            losses.textContent = 'Total Losses:'+ totalLoss + 1;
+            wins.textContent = 'Total Wins:'+ totalWins;
+         }
     })
 };
 
@@ -36,23 +60,12 @@ for (var i = 0; i < boxes.length; i ++ ){
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
 
-randomNumber = Math.floor(Math.random()*4);
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
 
-if(boxClicked = randomNumber){
-    message.textContent = "Congratulations!";
-    wins.textContent = totalWins + 1;
-}
+
 
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
-
-else {
-   for(var i = 0; i < losingMessages.length; i++){
-    message.textContent = losingMessages[i];
-   }
-   losses.textContent = totalLoss + 1;
-}
